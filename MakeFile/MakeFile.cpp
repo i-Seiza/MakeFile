fe6ce747db1d0a89562fcb83d9fe6c4324b66bf8
ikuko.masuda <ikuko.masuda@optim.co.jp>
2015-07-09 22:10:22 +0900

    printf関数で表示を出力するように修正 使っていない関数を削除

--- MakeFile/MakeFile.cpp
+++ MakeFile/MakeFile.cpp
@@ -25,33 +25,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		);
 	if (hFile==INVALID_HANDLE_VALUE)	return 0;
 
-	DWORD	dwRet;
-	DWORD	dwLen = 4096;		//十分なバッファー領域を確保
-	BYTE *pcbData = new BYTE[dwLen];
-	if(pcbData == NULL)
-	{
-		::CloseHandle(hFile);
-		return	false;
-	}
-	ZeroMemory(pcbData,dwLen);
-
-	STORAGE_PROPERTY_QUERY		sQuery;
-
-	sQuery.PropertyId	= StorageDeviceProperty;
-	sQuery.QueryType	= PropertyStandardQuery;
-	sQuery.AdditionalParameters[0] = NULL;
-	bool b = ::DeviceIoControl(hFile,IOCTL_STORAGE_QUERY_PROPERTY,&sQuery,sizeof(STORAGE_PROPERTY_QUERY),pcbData,dwLen,&dwRet,NULL);
-
-
-	STORAGE_DEVICE_DESCRIPTOR*	pDescriptor;
-	pDescriptor = (STORAGE_DEVICE_DESCRIPTOR*)pcbData;
-	delete(pcbData);
 
 	DRIVE_LAYOUT_INFORMATION_EX *driveLayoutInfo = new DRIVE_LAYOUT_INFORMATION_EX[4096];
 	
 	DWORD BytesReturned;
-	OVERLAPPED  Overlapped;
-	b = DeviceIoControl(
+	bool b = DeviceIoControl(
 		hFile,
 		IOCTL_DISK_GET_DRIVE_LAYOUT_EX, // dwIoControlCode
 		NULL,                           // lpInBuffer
@@ -63,14 +41,32 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	printf("PartitionCount: %d\n",driveLayoutInfo->PartitionCount);
-	printf("PartitionStyle: %d\n",driveLayoutInfo->PartitionStyle);
+	printf("PartitionStyle: %d\n", driveLayoutInfo->PartitionStyle);
+	printf("Mbr: %d\n", driveLayoutInfo->Mbr);
+	printf("Gpt: %d\n", driveLayoutInfo->Gpt);
 	printf("--------------\n");
 	for (int i = 0; i < driveLayoutInfo->PartitionCount; i++)
 	{
 		printf("パーティション番号:%d\n", i);
-		printf("%d\n", driveLayoutInfo->PartitionEntry[i].PartitionStyle);
+		printf("%PartitionStyle:%d\n", driveLayoutInfo->PartitionEntry[i].PartitionStyle);	//PARTITION_STYLE
+		printf("StartingOffset:%I64X\n", driveLayoutInfo->PartitionEntry[i].StartingOffset.QuadPart);	//LARGE_INTEGER
+		printf("PartitionLength:%I64X\n", driveLayoutInfo->PartitionEntry[i].PartitionLength.QuadPart);	//LARGE_INTEGER
+		printf("PartitionNumber:%08LX\n", driveLayoutInfo->PartitionEntry[i].PartitionNumber);	//DWORD
+		printf("RewritePartition:%d\n", driveLayoutInfo->PartitionEntry[i].RewritePartition);	//bool
+		printf("Mbr.PartitionType:%d\n", driveLayoutInfo->PartitionEntry[i].Mbr.PartitionType);	//PARTITION_INFORMATION_MBR.byte
+		printf("Mbr.BootIndicator:%d\n", driveLayoutInfo->PartitionEntry[i].Mbr.BootIndicator);	//PARTITION_INFORMATION_MBR.BootIndicator
+		printf("Mbr.RecognizedPartition:%d\n", driveLayoutInfo->PartitionEntry[i].Mbr.RecognizedPartition);	//PARTITION_INFORMATION_MBR.bool
+		printf("Mbr.HiddenSectors:%08LX\n", driveLayoutInfo->PartitionEntry[i].Mbr.HiddenSectors);	//PARTITION_INFORMATION_MBR.dword
+
+
+		//printf("Gpt.PartitionType:%d\n", driveLayoutInfo->PartitionEntry[i].Gpt.PartitionType);	//PARTITION_INFORMATION_GPT.GUID
+		//printf("Gpt.PartitionId:%d\n", driveLayoutInfo->PartitionEntry[i].Gpt.PartitionId);	//PARTITION_INFORMATION_GPT.GUID
+		printf("Gpt.Attributes:%d\n", driveLayoutInfo->PartitionEntry[i].Gpt.Attributes);	//PARTITION_INFORMATION_GPT.DWORD64
+		printf("Gpt.Name:%s\n", driveLayoutInfo->PartitionEntry[i].Gpt.Name);	//PARTITION_INFORMATION_GPT.wchar*
 
 	}
+
+	if (driveLayoutInfo)	delete []driveLayoutInfo;
 	
 
 	io.FileClose(&hFile);

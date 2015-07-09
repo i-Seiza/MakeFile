3fff5308f229b022e688c02f027f31cc7dbe347d
ikuko.masuda <ikuko.masuda@optim.co.jp>
2015-07-09 15:26:57 +0900

    IVdsVolumeMFを利用してドライブのフォーマットを実施

--- MakeFile/stdafx.h
+++ MakeFile/stdafx.h
@@ -6,7 +6,7 @@
 #pragma once
 
 #ifndef _WIN32_WINNT		// Windows XP 以降のバージョンに固有の機能の使用を許可します。                   
-#define _WIN32_WINNT 0x0501	// これを Windows の他のバージョン向けに適切な値に変更してください。
+#define _WIN32_WINNT 0x0601	// これを Windows の他のバージョン向けに適切な値に変更してください。
 #endif						
 
 #include <stdio.h>

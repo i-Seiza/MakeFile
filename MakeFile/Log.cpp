9573fcbaebd9e1d3d9f8e3d1f4cc9cf79223ae3b
ikuko.masuda <ikuko.masuda@optim.co.jp>
2015-07-15 16:39:43 +0900

    * 日本語のログを表示できるように修正  * 指定された場合はログ情報をテキストに吐き出すよう修正

--- MakeFile/Log.cpp
+++ MakeFile/Log.cpp
@@ -1,16 +1,43 @@
 #include "StdAfx.h"
 #include "Log.h"
 
+#define _LOG_FILE_NAME L"log.txt"
+
 /////////////////////////
 // コンストラクタ
 CLog::CLog(void)
 {
-
+	_tsetlocale(LC_ALL, _T(""));
+	SetWriteFile(false);
+}
+CLog::CLog(bool bFile)
+{
+	_tsetlocale(LC_ALL, _T(""));
+	SetWriteFile(bFile);
+	RefreshLogFile();
 }
 
 CLog::~CLog(void)
 {
 
+}
+
+void CLog::SetWriteFile( bool bStatus )
+{
+	m_bWriteFile = bStatus;
+}
+
+void CLog::RefreshLogFile()
+{
+	if(m_bWriteFile)
+	{
+		FILE *fp = NULL;
+		_wfopen_s( &fp, _LOG_FILE_NAME, L"w" );
+		if(fp)
+		{
+			fclose(fp);
+		}
+	}
 }
 
 /////////////////////////
@@ -20,6 +47,17 @@ void CLog::Log(std::wstring str)
 	str += L"\n";
 	::OutputDebugString(str.c_str());
 	wprintf(str.c_str());
+
+	if(m_bWriteFile)
+	{
+		FILE *fp = NULL;
+		_wfopen_s( &fp, _LOG_FILE_NAME, L"a" );
+		if(fp)
+		{
+			fputws( str.c_str(), fp );
+			fclose(fp);
+		}
+	}
 }
 
 
